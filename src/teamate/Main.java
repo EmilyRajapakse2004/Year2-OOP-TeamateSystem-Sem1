d1465e78fb4966a6e39b5ff5bde66035bdf95b72
EmilyRajapakse2004 <lidiyarajapakse@gmail.com>
2025-11-11 08:21:05 -0800

    commit

--- src/teamate/Main.java
+++ src/teamate/Main.java
@@ -1,13 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+package teamate;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String input = "participants_sample.csv";
+        String output = "formed_teams.csv";
+
+        // Allow CLI args to specify team size
+        int teamSize = 5;
+        if (args.length >= 1) {
+            try {
+                teamSize = Integer.parseInt(args[0]);
+            } catch (NumberFormatException ex) {
+                System.err.println("Invalid team size passed - using default 5");
+            }
+        }
+        System.out.println("Team size: " + teamSize);
+
+        try {
+            List<Participant> participants = CSVUtil.readParticipants(input);
+            System.out.println("Loaded participants: " + participants.size());
+            // print sample
+            // Build teams
+            TeamBuilder builder = new TeamBuilder(teamSize, 2, Math.min(3, teamSize));
+            List<Team> teams = builder.buildTeams(participants);
+
+            // Display teams summary
+            for (Team t : teams) {
+                System.out.println(t);
+                for (Participant p : t.getMembers()) {
+                    System.out.println("  - " + p);
+                }
+            }
+
+            CSVUtil.writeTeams(output, teams);
+            System.out.println("Formed teams written to " + output);
+        } catch (Exception ex) {
+            System.err.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
 }
