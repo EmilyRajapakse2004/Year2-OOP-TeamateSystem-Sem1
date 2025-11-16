dd7ff298184ac7363dbac8d64c55e66a3abed9dd
EmilyRajapakse2004 <lidiyarajapakse@gmail.com>
2025-11-16 15:32:59 +0530

    Changes to classes

--- src/teamate/Main.java
+++ src/teamate/Main.java
@@ -5,15 +5,6 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.*;
 
-/**
- * Main program:
- * - load participants_sample.csv (if user chooses)
- * - optionally add participants manually via survey (Q1..Q5 + game + role + skill)
- * - ask for team size (3..6)
- * - concurrently classify (ensure personality types)
- * - build balanced teams
- * - save formed_teams.csv
- */
 public class Main {
 
     public static void main(String[] args) {
